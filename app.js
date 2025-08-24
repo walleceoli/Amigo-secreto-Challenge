22d6c8b88da807ed3d60b33df9c2f857a1eae051
Walleceoli <wallece.oli@gmail.com>
2025-08-24 14:28:47 -0300

    AdcionandoValidaçõesETextosNaTela

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 
 let amigos = [];
+function exibirTextoNaTela(id, texto){
+    let campo = document.getElementById(id);
+    campo.innerHTML = texto;
+}
+function limparCampo(id){
+    nomes = document.getElementById(id);
+    nomes.value = '';
+}
+
 //Adcionando os nomes dos amigos a uma lista, não dar para adcionar o mesmo amigo duas vezes.
 function adicionarAmigo(){
-    let amigosInseridos =  document.querySelector('input').value;
-    if (amigosInseridos != amigos){
+    let amigosInseridos = document.querySelector('input').value;
+    if (amigosInseridos == '') {
+        exibirTextoNaTela("resultado",'Digite um nome válido!');
+    } if (amigosInseridos != amigos){
         amigos.push(amigosInseridos);
+        exibirTextoNaTela("listaAmigos",amigos);
+        limparCampo("amigo");
+        limparCampo("resultado");
     }else{
         alert('Erro');
     }
     console.log(amigosInseridos);
+    return amigosInseridos;
 }
