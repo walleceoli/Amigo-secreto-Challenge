5d44fff961df936f6464ed04927bb34978e348c9
Walleceoli <wallece.oli@gmail.com>
2025-08-24 23:43:24 -0300

    Ajustei a função de adcionar amigo e adcionei a função de sortear amigos

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 
 let amigos = [];
+
 function exibirTextoNaTela(id, texto){
     let campo = document.getElementById(id);
     campo.innerHTML = texto;
 }
+
 function limparCampo(id){
-    nomes = document.getElementById(id);
+    let nomes = document.getElementById(id);
     nomes.value = '';
 }
 
 //Adcionando os nomes dos amigos a uma lista, não dar para adcionar o mesmo amigo duas vezes.
 function adicionarAmigo(){
     let amigosInseridos = document.querySelector('input').value;
-    if (amigosInseridos == '') {
+    if (amigosInseridos == "") {
         exibirTextoNaTela("resultado",'Digite um nome válido!');
-    } if (amigosInseridos != amigos){
+        limparCampo("amigo");
+        return;
+    } if (!amigos.includes(amigosInseridos)){
         amigos.push(amigosInseridos);
         exibirTextoNaTela("listaAmigos",amigos);
+        exibirTextoNaTela("resultado","");
         limparCampo("amigo");
-        limparCampo("resultado");
     }else{
-        alert('Erro');
+        exibirTextoNaTela("resultado",'Nome já esta na lista');
+        limparCampo("amigo");
     }
-    console.log(amigosInseridos);
     return amigosInseridos;
+}
+function sortearAmigo(){
+    if (amigos.length >  1){
+       let indiceSorteado = Math.floor(Math.random()*amigos.length);
+       let amigoSorteado = amigos[indiceSorteado];
+       exibirTextoNaTela("resultado",`${amigoSorteado} foi o amigo sorteado!`);
+    }else{
+        exibirTextoNaTela("resultado",'Adcione mais amigos!');
+    }
+
 }
