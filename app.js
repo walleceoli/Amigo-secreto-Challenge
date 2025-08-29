87210cc64c34d2f5be8165a7bc130bc26e797913
Walleceoli <wallece.oli@gmail.com>
2025-08-28 23:57:00 -0300

    Adcionei a função render lista para trasformar cada nome em um item da lista e um novo parametro na função exibirTextoNaTela par definir a cor do resultado

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 let amigos = [];
 
 //Função para exibir o texto desejado na tela puxando o id do HTML.
-function exibirTextoNaTela(id, texto){
+//Adcionei um novo parametro onde dependendo do resultado a cor do resultado é alterada.
+function exibirTextoNaTela(id, texto, cor = "green"){
     let campo = document.getElementById(id);
     campo.innerHTML = texto;
+    campo.style.color = cor;
+}
+
+//Função que percorre a lista amigos e cria um <li> para cada nome,
+//depois junta todos os <li> em uma unica string e insere dentro da ul id="listaAmigos".
+//Isso faz com que cada amigo seja um item da lista, e fique um abaixo do outro, não na frente separado por virgula.
+function renderLista(){
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML =  amigos.map(nome => `<li>${nome}</li>`).join("");
 }
 
 //Função utilizada para limpar os campos desejados.
@@ -17,16 +27,16 @@ function limparCampo(id){
 function adicionarAmigo(){
     let amigosInseridos = document.querySelector('input').value;
     if (amigosInseridos == "") {
-        exibirTextoNaTela("resultado",'Digite um nome válido!');
+        exibirTextoNaTela("resultado",'Digite um nome válido!',"red");
         limparCampo("amigo");
         return;
     } if (!amigos.includes(amigosInseridos)){
         amigos.push(amigosInseridos);
-        exibirTextoNaTela("listaAmigos",amigos);
-        exibirTextoNaTela("resultado","");
+        renderLista();
+        exibirTextoNaTela("resultado","","green");
         limparCampo("amigo");
     }else{
-        exibirTextoNaTela("resultado",'Nome já esta na lista');
+        exibirTextoNaTela("resultado",'Nome já esta na lista',"red");
         limparCampo("amigo");
     }
     return amigosInseridos;
